41245dec72ce6305f9a794c57459da8f198d898d
user <naufalyafi0206@gmail.com>
2025-02-11 07:44:15 +0700

    Update Java_gopal.java

--- java_gopal/src/java_gopal/Java_gopal.java
+++ java_gopal/src/java_gopal/Java_gopal.java
@@ -5,6 +5,7 @@ public class Java_gopal {
   
     public static void main(String[] args) {
         System.out.println("HAI INI ADALAH NAUFAL YAFI ALIFUDIN");
+        System.out.println("SAYA DARI GOLONGAN A ");
     }
     
 }
