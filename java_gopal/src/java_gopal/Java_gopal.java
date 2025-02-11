dd05c6a221fb7ab8d2f95fdde97781663db5e94e
user <naufalyafi0206@gmail.com>
2025-02-11 08:12:16 +0700

    anang

--- java_gopal/src/java_gopal/Java_gopal.java
+++ java_gopal/src/java_gopal/Java_gopal.java
@@ -6,6 +6,7 @@ public class Java_gopal {
     public static void main(String[] args) {
         System.out.println("HAI INI ADALAH NAUFAL YAFI ALIFUDIN");
         System.out.println("SAYA DARI GOLONGAN A ");
+        System.out.println("ANANG MAQQRUF");
     }
     
 }
