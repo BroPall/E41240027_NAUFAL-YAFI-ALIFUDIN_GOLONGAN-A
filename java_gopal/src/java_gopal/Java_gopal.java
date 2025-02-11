3abbf55e668c8cf10e93046c02190653552440b2
user <naufalyafi0206@gmail.com>
2025-02-11 07:42:43 +0700

    hai

---
+++ java_gopal/src/java_gopal/Java_gopal.java
@@ -0,0 +1,10 @@
+package java_gopal;
+
+public class Java_gopal {
+
+  
+    public static void main(String[] args) {
+        System.out.println("HAI INI ADALAH NAUFAL YAFI ALIFUDIN");
+    }
+    
+}
