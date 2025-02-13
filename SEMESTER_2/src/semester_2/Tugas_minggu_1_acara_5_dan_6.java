ce4864c33c9c6e8f12f0a7b199ae25a6e26b1031
user <naufalyafi0206@gmail.com>
2025-02-13 08:46:21 +0700

    hi

---
+++ SEMESTER_2/src/semester_2/Tugas_minggu_1_acara_5_dan_6.java
@@ -0,0 +1,15 @@
+package semester_2;
+
+public class Tugas_minggu_1_acara_5_dan_6 {
+
+    public static void main(String[] args) {
+        String fName = "Lisa";
+        String IName = "Palombo";
+        int stuld = 123456789;
+        String stuStatus = "Active";
+        System.out.println(fName);
+        System.out.println(IName);
+        System.out.println(stuld);
+        System.out.println(stuStatus);
+    }
+}
