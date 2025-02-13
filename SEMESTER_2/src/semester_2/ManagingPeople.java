903c3a41264e6f80ce321d9a78cf4c058a7817fd
user <naufalyafi0206@gmail.com>
2025-02-13 10:48:28 +0700

    acc bangku

---
+++ SEMESTER_2/src/semester_2/ManagingPeople.java
@@ -0,0 +1,15 @@
+package semester_2;
+
+public class ManagingPeople {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Arial", 37);
+        Person p2 = new Person("Jashon", 15);
+        
+       if (p1.getAge()==p1.getAge()) {
+           System.out.println(p1.getName()+ " Adalah nama yang benar, serta umurnya adalah : " + p1.getAge());
+       }else{
+           System.out.println(p1.getName()+ " Adalah nama yang salah, serta umurnya adalah : " + p2.getAge());
+       }  
+}
+}
--- SEMESTER_2/src/semester_2/SEMESTER_2.java
+++
@@ -1,9 +0,0 @@
-package semester_2;
-
-public class SEMESTER_2 {
-
-    public static void main(String[] args) {    
-        
-    }
-    
-}
