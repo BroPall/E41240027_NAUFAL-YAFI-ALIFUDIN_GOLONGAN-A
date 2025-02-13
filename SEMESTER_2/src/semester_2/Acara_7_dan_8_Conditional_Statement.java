4b172881e57432d076c83205fc025caceb44a960
user <naufalyafi0206@gmail.com>
2025-02-13 15:58:49 +0700

    acc

---
+++ SEMESTER_2/src/semester_2/Acara_7_dan_8_Conditional_Statement.java
@@ -0,0 +1,47 @@
+package semester_2;
+
+import java.util.Scanner;
+
+public class Acara_7_dan_8_Conditional_Statement {
+
+    public static void main(String[] args) {
+//     membuat variabel 
+        int harga = 0;
+        int total;
+        String nama;
+        Scanner scan = new Scanner(System.in);
+        Scanner can = new Scanner(System.in);
+//     mengambil input
+
+        System.out.println("----------------------------------------------");
+        System.out.println("     Kharisma Agung PLaza < KAP > ");
+        System.out.println("        Promo Belanja berhadiah");
+        System.out.println("  Khusus Pembelian 5 Barang Pertama");
+        System.out.println("   Dengan Harga minimum Rp 10000,00");
+        System.out.println("----------------------------------------------");
+        System.out.print("Masukkan nama pembeli : ");
+        nama = scan.next();
+
+        System.out.print("Masukkan harga barang ke-1 : ");
+        harga += can.nextInt();
+        System.out.print("Masukkan harga barang ke-2 : ");
+        harga += can.nextInt();
+        System.out.print("Masukkan harga barang ke-3 : ");
+        harga += can.nextInt();
+        System.out.print("Masukkan harga barang ke-4 : ");
+        harga += can.nextInt();
+        System.out.print("Masukkan harga barang ke-5 : ");
+        harga += can.nextInt();
+        System.out.println("Total harga pembelian atas nama " + nama + " adalah " + " Rp. " + harga);
+
+            System.out.println("Selamat.... ");
+            System.out.println("Anda mendapat hadiah 1 buah mug cantik");
+
+            System.out.println("----------------------------------------------");
+        
+
+        System.out.println("             Terima Kasih    ");
+        System.out.println(" Anda sudah belanja di Kharisma Agung Plaza");
+
+    }
+}
---
+++ SEMESTER_2/src/semester_2/Acara_7_dan_8_Conditional_Statement_2.java
@@ -0,0 +1,70 @@
+package semester_2;
+
+import java.util.Scanner;
+
+public class Acara_7_dan_8_Conditional_Statement_2 {
+
+    public static void main(String[] args) {
+        String minuman;
+        int angka;
+        String nama;
+
+        Scanner scan = new Scanner(System.in);
+        Scanner can = new Scanner(System.in);
+
+        System.out.println("                CAFE CERIA");
+        System.out.println("               ANEKA MINUMAN");
+        System.out.println("----------------------------------------------");
+        System.out.println("              SPESIAL MENU :");
+        System.out.println("             1. Soft Drinks");
+        System.out.println("             2. Mix Juice");
+        System.out.println("             3. Nescafe");
+        System.out.println("             4. Soda milk");
+        System.out.println("             5. Tea");
+        System.out.println("----------------------------------------------");
+
+        System.out.print("Masukkan nama pembeli : ");
+        nama = scan.nextLine();
+
+        System.out.print("Silahkan masukkan pilihan anda : ");
+        angka = can.nextInt();
+
+        switch (angka) {
+            case 0:
+                System.out.println("Minuman yang anda pesan tidak tersedia pada Cafe kami");
+                System.out.println("Terima Kasih " + nama + " telah berkunjung di Cafe Ceria");
+                break;
+            case 1:
+                System.out.println("Minuman yang anda pesan adalah Soft Drinks");
+                System.out.println("Pesanan akan segera kami antar");
+                System.out.println("Terima Kasih " + nama + " telah berkunjung di Cafe Ceria");
+                break;
+            case 2:
+                System.out.println("Minuman yang anda pesan adalah Mix Juice");
+                System.out.println("Pesanan akan segera kami antar");
+                System.out.println("Terima Kasih " + nama + " telah berkunjung di Cafe Ceria");
+                break;
+            case 3:
+                System.out.println("Minuman yang anda pesan adalah Nescafe");
+                System.out.println("Pesanan akan segera kami antar");
+                System.out.println("Terima Kasih " + nama + " telah berkunjung di Cafe Ceria");
+                break;
+            case 4:
+                System.out.println(" Minuman yang anda pesan adalah Soda milk");
+                System.out.println("Pesanan akan segera kami antar");
+                System.out.println("Terima Kasih " + nama + " telah berkunjung di Cafe Ceria");
+                break;
+            case 5:
+                System.out.println("Minuman yang anda pesan adalah Tea");
+                System.out.println("Pesanan akan segera kami antar");
+                System.out.println("Terima Kasih " + nama + " telah berkunjung di Cafe Ceria");
+                break;
+            case 6:
+                System.out.println("Minuman yang anda pesan tidak tersedia pada Cafe kami");
+                System.out.println("Terima Kasih " + nama + " telah berkunjung di Cafe Ceria");
+                break;
+        }
+
+    }
+
+}
