f6cfd27b2a52ef08129fe5bd336543e366e0a1f0
user <naufalyafi0206@gmail.com>
2025-02-13 14:33:42 +0700

    qq

---
+++ SEMESTER_2/src/semester_2/coba.java
@@ -0,0 +1,22 @@
+package semester_2;
+
+public class coba {
+
+    public static void main(String[] args) {
+       boolean gameOver = false;
+       int student = 50, classes = 3;
+       double sales_tax;
+       short number1;
+       
+       
+       int iya = 200; 
+       float priceindex;
+       double lastYearsPrice; 
+       long klass;   // class termasuk syntax java
+//yang mana langsung menggunakkan inisiasi sebagai deklarasi, dan jika kita menambahkan deklarasi sebelumnya semisal contoh int iya = 2beOrNot2be; maka tetap akan terjadi error karena int hanyalah angka, jadi untuk inisiasinya harus angka seperti  int iya = 200;
+//karena pada pendekalrasiaannya ditambahkan spasi, yang dimana penyebab terjadinya error adalah pada spasi berikut, dengan kita memperbaikinya dengan cara seperti kita membuatnya seperti ini A_B ataupun AB atau juga bisa menjadi A saja jika ingin satu deklarasi   
+//Jika terdapat tanda petik di atas, maka akan dianggap sebagai string. Entah tanda petiknya 1 maupun 2 
+
+    }
+    
+}
