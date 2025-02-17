03b5884cf37e6caaa0bc538fdc4a56c58b127fba
user <naufalyafi0206@gmail.com>
2025-02-17 14:50:12 +0700

    gg

--- SEMESTER_2/src/semester_2/Tugas_minggu3_Acara_9_10.java
+++ SEMESTER_2/src/semester_2/Tugas_minggu3_Acara_9_10.java
@@ -16,7 +16,7 @@ public class Tugas_minggu3_Acara_9_10 {
         
         for (int k = batasAwal; k <= batasAkhir; k++) {
         if (k % 2 == 0){
-            System.out.print(k + " ");
+            System.out.println(k + " ");
         }
     }
     }
--- SEMESTER_2/src/semester_2/Tugas_minggu3_Acara_9_10_ke2.java
+++ SEMESTER_2/src/semester_2/Tugas_minggu3_Acara_9_10_ke2.java
@@ -5,7 +5,7 @@ public class Tugas_minggu3_Acara_9_10_ke2 {
     public static void main(String[] args) {
         int i = 1; 
 
-        System.out.println("          Do__While");
+        System.out.println("         Do__While");
         System.out.println("Bilangan kelipatan 2 (1-100)");
         System.out.println("============================");
 
