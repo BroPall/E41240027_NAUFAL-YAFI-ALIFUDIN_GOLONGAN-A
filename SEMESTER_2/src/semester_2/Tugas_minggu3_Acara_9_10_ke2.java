179f8bb24c27b18019647cc554cdf82627b7f991
user <naufalyafi0206@gmail.com>
2025-02-17 13:00:18 +0700

    Create Tugas_minggu3_Acara_9_10_ke2.java

---
+++ SEMESTER_2/src/semester_2/Tugas_minggu3_Acara_9_10_ke2.java
@@ -0,0 +1,17 @@
+package semester_2;
+
+public class Tugas_minggu3_Acara_9_10_ke2 {
+
+    public static void main(String[] args) {
+        int i = 1; 
+
+        System.out.println("          Do__While");
+        System.out.println("Bilangan kelipatan 2 (1-100)");
+        System.out.println("============================");
+
+        do {
+            System.out.println(i);
+            i *= 2; 
+        } while (i <= 100); 
+    }
+}
