498e9ad8f93df8de7b8f2b15c70be082787e76c6
user <naufalyafi0206@gmail.com>
2025-03-03 07:13:06 +0700

    1

---
+++ SEMESTER_2/src/Pewarisan/DynamicDispath.java
@@ -0,0 +1,42 @@
+package Pewarisan;
+
+    class A {
+
+        void callthis() {
+            System.out.println("Inside Class A's Method!! ");
+        }
+    }
+
+    class B extends A {
+
+        void callthis() {
+            System.out.println("Inside Class B's Method!! ");
+        }
+    }
+
+    class C extends A {
+
+        void callthis() {
+            System.out.println("Inside Class C's Method!! ");
+        }
+    }
+
+     class DynamicDispath {
+
+        public static void main(String[] args) {
+            A a = new A();
+            B b = new B();
+            C c = new C();
+            A ref;
+
+            ref = b;
+            ref.callthis();
+
+            ref = c;
+            ref.callthis();
+
+            ref = a;
+            ref.callthis();
+        }
+    }
+
